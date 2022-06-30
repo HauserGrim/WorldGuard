f9681ffe0edca01e77da6b6e358d5629aecb1aca
HauserGrim <cool.vitaminka1@yandex.ru>
2022-07-01 00:37:08 +0300

    Основано на переводе DarkFort
    Сухой перевод, основанный на переводе DarkFort с некоторыми исправлениями и изменениями.
    Без красивостей, они чуть позже.
    
    Изменено 1 слово
    
    Пара строк

--- worldguard-bukkit/src/main/java/com/sk89q/worldguard/bukkit/listener/BlockedPotionsListener.java
+++ worldguard-bukkit/src/main/java/com/sk89q/worldguard/bukkit/listener/BlockedPotionsListener.java
@@ -81,8 +81,8 @@ public class BlockedPotionsListener extends AbstractListener {
                         if (getPlugin().hasPermission(player, "worldguard.override.potions")) {
                             return;
                         }
-                        player.sendMessage(ChatColor.RED + "Sorry, arrows with "
-                                + blockedEffect.getName() + " are presently disabled.");
+                        player.sendMessage(ChatColor.RED + "К сожалению, стрелы с "
+                                + blockedEffect.getName() + " в настоящее время отключены.");
                     }
                     event.setCancelled(true);
                 }
@@ -133,15 +133,15 @@ public class BlockedPotionsListener extends AbstractListener {
                     if (getPlugin().hasPermission(player, "worldguard.override.potions")) {
                         if (wcfg.blockPotionsAlways && (item.getType() == Material.SPLASH_POTION
                                 || item.getType() == Material.LINGERING_POTION)) {
-                            player.sendMessage(ChatColor.RED + "Sorry, potions with " +
-                                    blockedEffect.getName() + " can't be thrown, " +
-                                    "even if you have a permission to bypass it, " +
-                                    "due to limitations (and because overly-reliable potion blocking is on).");
+                            player.sendMessage(ChatColor.RED + "К сожалению, стрелы с " +
+                                    blockedEffect.getName() + " нельзя бросить, " +
+                                    "даже если у Вас есть разрешение обойти это, " +
+                                    "из-за ограничений (и из-за чрезмерно надежной блокировки зелья).");
                             event.setCancelled(true);
                         }
                     } else {
-                        player.sendMessage(ChatColor.RED + "Sorry, potions with "
-                                + blockedEffect.getName() + " are presently disabled.");
+                        player.sendMessage(ChatColor.RED + "К сожалению, стрелы с "
+                                + blockedEffect.getName() + " в настоящее время отключены..");
                         event.setCancelled(true);
                     }
                 } else {
